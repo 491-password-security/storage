3099b92107abf3e707bfe3802bce4cfdbac0c4dc
Can <canardaaydin@gmail.com>
2021-12-21 15:18:02 +0300

    [feat] reverted endpoints

--- nodejs/server.js
+++ nodejs/server.js
@@ -3,6 +3,8 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const crypto = require('crypto-helper-ku');
 const port = process.env.PORT || 3000;
+const {MongoClient} = require('mongodb');
+
 
 const Number = crypto.Number;
 const OPRF_KEY = [
@@ -325,77 +327,50 @@ http.listen(port, () => {
 
 // 'use strict';
 
-// const app = require('express')();
-// const crypto = require('crypto-helper-ku');
-// const http = require('http').Server(app);
-// const {MongoClient} = require('mongodb');
-// const io = require("socket.io")(http);
-
-// const PORT = 8080;
-// const HOST = 'localhost';
 
 // // const uri = "mongodb://mongo:mongo@mongo:27017/?authSource=admin";
-// const uri = process.env.MONGO_URI;
+const uri = process.env.MONGO_URI;
 // console.log(uri);
 
-// const client = new MongoClient(uri);
-// var users;
-// var col;
+const client = new MongoClient(uri);
+var users;
+var col;
 
-// (async () => {
-//     try {
-//         await client.connect();
-//         console.log("connected");
-//         const db = client.db('s3');
-//         col = db.collection('s4');
-//         users = await col.find({}).toArray();
-//         console.log(users);
-//     } catch (err) {
-//         console.error(err);
-//     }
-// })()
+(async () => {
+    try {
+        await client.connect();
+        console.log("connected");
+        const db = client.db('s3');
+        col = db.collection('s4');
+        users = await col.find({}).toArray();
+        console.log(users);
+    } catch (err) {
+        console.error(err);
+    }
+})()
 
 
-// // App
-// app.get('/save-password-share/:key/:value/:iv', (req, res) => {
+// App
+app.get('/save-password-share/:key/:value/:iv', (req, res) => {
     
-//     col.insertOne({
-//         key: req.params.key,
-//         value: req.params.value,
-//         iv: req.params.iv
-//     });
+    col.insertOne({
+        key: req.params.key,
+        value: req.params.value,
+        iv: req.params.iv
+    });
 
-//     res.send(req.params.key + ':' + req.params.value + ':' + req.params.iv);
-// });
+    res.send(req.params.key + ':' + req.params.value + ':' + req.params.iv);
+});
 
-// app.get('/get-password-share/:key', (req, res) => {
+app.get('/get-password-share/:key', (req, res) => {
 
-//     col.findOne({
-//         key: req.params.key
-//     }, (err, doc) => {
-//         if (err) {
-//             console.log(err);
-//         } else {
-//             res.send(doc.value + ':' + doc.iv);
-//         }
-//     });
-// });
-
-// io.on("connection", socket => {
-//     // or with emit() and custom event names
-//     console.log('connectedconnectedconnectedconnectedconnectedconnectedconnectedconnectedconnectedconnectedconnectedconnectedconnectedconnectedconnectedconnectedconnected')
-//     console.log(socket.id)
-//     // socket.emit("greetings", "Hey!", { "ms": "jane" }, Buffer.from([4, 3, 3, 1]));
-  
-//     // handle the event sent with socket.emit()
-//     // socket.on("salutations", (elem1, elem2, elem3) => {
-//     //   console.log(elem1, elem2, elem3);
-//     // });
-  
-//     // socket.on("initOT", () => {
-  
-//     // })
-// });
-
-// http.listen(PORT, HOST);
-// console.log(`Running on http://${HOST}:${PORT}`);
+    col.findOne({
+        key: req.params.key
+    }, (err, doc) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(doc.value + ':' + doc.iv);
+        }
+    });
+});
